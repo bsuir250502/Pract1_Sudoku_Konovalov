e82e1d44da4cd63e8f6e8e526191ece4ad24d55a
Ales Guzik <me@aguzik.net>
2013-03-06 18:26:49 +0300

    fixed indent-related problem

--- custom_tools.h
+++ custom_tools.h
@@ -1,21 +1,21 @@
 #include "stdio.h"
 #include "stdlib.h"
 #include "string.h"
-typedef struct argv_  {
-    char *path;
-     char *custom_conf_location;
-     int help;
-     int output_type;
-     char *output_file;
-} argv_t;
-typedef struct cell_  {
-    int num, poss_count;
-     int poss[9];
-} cell_t;
+typedef struct argv_ {
+    char *path;
+    char *custom_conf_location;
+    int help;
+    int output_type;
+    char *output_file;
+} argv_t;
+typedef struct cell_ {
+    int num, poss_count;
+    int poss[9];
+} cell_t;
 
 #ifndef C_SUDOKU_TOOLS_H
 #define C_SUDOKU_TOOLS_H
-int getopt_c(int, char **, argv_t *);
-int read_conf_file(cell_t **, char *);
-
+int getopt_c(int, char **, argv_t *);
+int read_conf_file(cell_t **, char *);
+
 #endif
