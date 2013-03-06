811244ff0e0ec22cb1dff3467327977672f71f05
Anton Kanavalau <akannwetr@gmail.com>
2013-03-06 23:39:19 +0300

    remade command line options, added options function, custom config name/location supported(maybe?)

--- custom_tools.c
+++ custom_tools.c
@@ -1,130 +1,149 @@
 #include "custom_tools.h"
 #define C_SUDOKU_TOOLS_H
- typedef struct com_line_options  {
-    int type;                  /*0-option,1-argument */
-     char opt;
-     char *value;
-} cl_opt;
-int getopt_out(int, cl_opt *, argv_t *);
-int read_conf_file(cell_t ** table, char *path) 
+
+int display_usage(void);
+
+int read_conf_file(cell_t ** table, char *path)
 {
-    FILE * conf;
-    int i = 0, chk = 0;
-    int cell_prop[3] = { 0, 0, 0 };
-    char param;
-    chk = strlen(path);
-    while (*(path + chk - i) != '/' && *(path + chk - i) != '\\')
-        i++;
-    path = path + chk - i + 1;
-    path = strncpy(path, "config\0", 7);
-    path = path - chk + i - 1;
-    i = 0;
-    chk = 0;
-    conf = fopen(path, "rt");
-    if (!conf)
-         {
-        printf
+    FILE *conf;
+    int i = 0, chk = 0;
+    int cell_prop[3] = { 0, 0, 0 };
+    char param;
+    i = 0;
+    chk = 0;
+    conf = fopen(path, "rt");
+    if (!conf) {
+        printf
             ("Config file is missing, ensure that 'config' file is in executable location.\n");
-        return 0;
-        }
-    
-    do
-         {
-        fscanf(conf, "%c", &param);
-        if ((param == ',' || param == ';') && cell_prop[2] && i > 2)
-             {
-            i = 0;
-            table[cell_prop[0]][cell_prop[1]].num = cell_prop[2];
-            cell_prop[2] = 0;
-            }
-        if ((chk = param - '0') >= 0 && (chk = param - '0') <= 9)
-             {
-            if (i > 2)
-                 {
-                printf
+        return 0;
+    }
+    do {
+        fscanf(conf, "%c", &param);
+        if ((param == ',' || param == ';') && cell_prop[2] && i > 2) {
+            i = 0;
+            table[cell_prop[0]][cell_prop[1]].num = cell_prop[2];
+            cell_prop[2] = 0;
+        }
+        if ((chk = param - '0') >= 0 && (chk = param - '0') <= 9) {
+            if (i > 2) {
+                printf
                     ("Bad config file, see -h or --help for more info.\n");
-                fclose(conf);
-                return 0;
-                }
-            if (i < 2)
-                cell_prop[i] = param - '0' - 1;
-            
+                fclose(conf);
+                return 0;
+            }
+            if (i < 2)
+                cell_prop[i] = param - '0' - 1;
             else
-                cell_prop[i] = param - '0';
-            i++;
-            }
-    } while (param != ';');
-    fclose(conf);
-    return 1;
-}
+                cell_prop[i] = param - '0';
+            i++;
+        }
+    }
+    while (param != ';');
+    fclose(conf);
+    return 1;
+}
 
-int getopt_c(int argc, char **argv, argv_t * Arguments) /*Will be remade */ 
+int options(argv_t * Arguments, int argc, char **argv)
 {
-    int i;
-    cl_opt * Parameters;
-    Parameters = (cl_opt *) calloc(argc - 1, sizeof(Parameters));
-    for (i = 1; i < argc; i++)
-        Parameters[i].value =
-            (char *) calloc(strlen(*(argv + i)), sizeof(char));
-    strncpy(Arguments->path, *argv, strlen(*argv));
-    if (argc < 2)
-        printf
-            ("No arguments found. Are you really thinking help will display itself? Use -h or --help.\n");
-    for (i = 1; i < argc; i++)
-        if (**(argv + i) == '-')
-             {
-            Parameters[i - 1].type = 0;
-            if (*(*(argv + i) + 1) == '-')
-                Parameters[i - 1].opt = *(*(argv + i) + 2);
-            
-            else
-                Parameters[i - 1].opt = *(*(argv + i) + 1);
-            }
-    
-        else
-             {
-            Parameters[i - 1].type = 1;
-            sscanf(*(argv + i), "%s", Parameters[i - 1].value);
-            }
-    if (!getopt_out(argc, Parameters, Arguments))
-        return 0;
-    return 1;
-}
+    int no_arguments_needed = 1, i = 0, path_set = 0, len = 0;
+    char *arg;
+    char *destination = NULL;
+    if (argc < 2)
+        printf("No arguments found. Use -h or --help.\n");
+    for (i = 1; i < argc; i++) {
+        arg = *(argv + i);
+        switch (getopt_c(arg)) {
+        case 0:
+            if (no_arguments_needed) {
+                printf("Unknown command %s.\n", arg);
+                return 0;
+            }
+            strncpy(destination, arg, strlen(arg));
+            no_arguments_needed = 1;
+            destination = NULL;
+            break;
+        case 1:
+            if (no_arguments_needed == 0)
+                return 0;
+            switch (*arg) {
+            case 'h':
+                no_arguments_needed = 1;
+                display_usage();
+                break;
+            case 'l':
+                no_arguments_needed = 0;
+                destination = Arguments->custom_conf_location;
+                path_set = 1;
+                break;
+            case 'o':
+                no_arguments_needed = 0;
+                destination = Arguments->output_file;
+                Arguments->output_type = 1;
+                break;
+            default:
+                printf("Unknown command -%s.\n", arg);
+                return 0;
+                break;
+            }
+            break;
+        case 2:
+            if (no_arguments_needed == 0)
+                return 0;
+            if (!strcmp(arg, "help"))
+                display_usage();
+            else if (!strcmp(arg, "config-location")) {
+                destination = Arguments->custom_conf_location;
+                no_arguments_needed = 0;
+                path_set = 1;
+            } else if (!strcmp(arg, "output")) {
+                no_arguments_needed = 0;
+                destination = Arguments->output_file;
+            } else {
+                printf("Unknown command --%s.\n", arg);
+                return 0;
+            }
+            break;
+        default:
+            printf("Unknown command %s.\n", arg);
+            return 0;
+            break;
+        }
+    }
+    if (!path_set) {
+        i = 0;
+        len = strlen(*argv);
+        strncpy(Arguments->custom_conf_location, *argv, strlen(*argv));
+        while (*(Arguments->custom_conf_location + len - i) != '/'
+               && *(Arguments->custom_conf_location + len - i) != '\\')
+            i++;
+        Arguments->custom_conf_location =
+            Arguments->custom_conf_location + len - i + 1;
+        Arguments->custom_conf_location =
+            strncpy(Arguments->custom_conf_location, "config\0", 7);
+        Arguments->custom_conf_location =
+            Arguments->custom_conf_location - len + i - 1;
+    }
+    return 1;
+}
 
-int getopt_out(int argc, cl_opt * Parameters, argv_t * Arguments) 
+int getopt_c(char *arg)
 {
-    int i = 0;
-    for (i = 0; i < argc; i++)
-        switch (Parameters[i].type)
-             {
-        case 0:
-            switch (Parameters[i].opt)
-                 {
-            case 'h':
-                Arguments->help = 1;
-                break;
-            case 'l':
-                if (Parameters[i + 1].value)
-                    Arguments->custom_conf_location =
-                        Parameters[i + 1].value;
-                break;
-            case 'c':
-                if (Parameters[i + 1].value)
-                    Arguments->custom_conf_location =
-                        Parameters[i + 1].value;
-                break;
-            case 'o':
-                if (Parameters[i + 1].value)
-                    Arguments->output_file = Parameters[i + 1].value;
-                break;
-                }
-        case 1:
-            if ((Parameters[i - 1].opt!='l' || Parameters[i - 1].opt!='c' || Parameters[i - 1].opt!='o') && i-1>0)
-                 {
-                printf("Unknown command %s", Parameters[i].value);
-                return 0;
-                }
-            break;
-            }
-    return 1;
-}
+    int arg_type = 0, i = 1;
+    while (*arg == '-') {
+        arg_type++;
+        *(arg) = *(arg + 1);
+        while (*(arg + i - 1)) {
+            *(arg + i) = *(arg + i + 1);
+            i++;
+        }
+        i = 1;
+    }
+    return arg_type;
+}
+
+int display_usage(void)
+{
+    printf
+        ("Usage: sudoku [options]\n -h --help                   Displays usage information\n -l --config_location <path> Defines config location(default is program directory)\n -o --output <path>          Sets output file name and location\n");
+    return 1;
+}
--- sudoku.c
+++ sudoku.c
@@ -1,52 +1,55 @@
 #include "custom_tools.h"
- int analysis_rowsNcolumns(cell_t **);
-int analysis_squareNode(cell_t **);
 
-/*int advanced_analyzis (cell_t**);*/ 
-int main(int argc, char **argv) 
-{
-    argv_t Arguments;
-    cell_t ** table;
-    int i = 0, j = 0;
-    Arguments.path = (char *) calloc(255, sizeof(char));
-    Arguments.output_file = (char *) calloc(255, sizeof(char));
-    if (!getopt_c(argc, argv, &Arguments))
-        return 0;
-    table = (cell_t **) calloc(9, sizeof(cell_t *));
-    for (i = 0; i < 9; i++)
-        *(table + i) = (cell_t *) calloc(9, sizeof(cell_t));
-    if (!read_conf_file(table, Arguments.path))
-        printf("Error in reading config file.\n");
-    for (i = 0; i < 9; i++)
-        for (j = 0; j < 9; j++)
-             {
-            printf(" %d ", table[i][j].num);
-            if (j == 8)
-                printf("\n");
-            }
-    return (0);                /* I like it */
-}
+int analysis_rowsNcolumns(cell_t **);
+int analysis_squareNode(cell_t **);
+/*int advanced_analyzis (cell_t**);*/
 
-int analysis_rowsNcolumns(cell_t ** table) 
+int main(int argc, char **argv)
 {
-    int i = 0, j = 0, node_val = 0, a_ = 0, b_ = 0;
-    node_val = table[a_][b_].num;
-    if (!table[a_][b_].num)
-         {
-        for (j = 0; j < 9; j++)
-            if (table[a_][j].num)
-                table[a_][b_].poss[table[a_][j].num - 1] =
+    argv_t Arguments;
+    cell_t **table;
+    int i = 0, j = 0;
+    Arguments.output_file = (char *) calloc(255, sizeof(char));
+    Arguments.custom_conf_location = (char *) calloc(255, sizeof(char));
+    if (!options(&Arguments, argc, argv)) {
+        printf("Error in parsing parameters.\n");
+        return 0;
+    }
+    table = (cell_t **) calloc(9, sizeof(cell_t *));
+    for (i = 0; i < 9; i++)
+        *(table + i) = (cell_t *) calloc(9, sizeof(cell_t));
+    if (!read_conf_file(table, Arguments.custom_conf_location))
+        printf("Error in reading config file.\n");
+    for (i = 0; i < 9; i++)
+        for (j = 0; j < 9; j++) {
+            printf(" %d ", table[i][j].num);
+            if (j == 8)
+                printf("\n");
+        }
+    return (0);                 /* I like it */
+}
+
+
+
+int analysis_rowsNcolumns(cell_t ** table)
+{
+    int i = 0, j = 0, node_val = 0, a_ = 0, b_ = 0;
+    node_val = table[a_][b_].num;
+    if (!table[a_][b_].num) {
+        for (j = 0; j < 9; j++)
+            if (table[a_][j].num)
+                table[a_][b_].poss[table[a_][j].num - 1] =
                     table[a_][j].num;
-        for (i = 0; i < 9; i++)
-            if (table[i][b_].num)
-                table[a_][b_].poss[table[i][b_].num - 1] =
+        for (i = 0; i < 9; i++)
+            if (table[i][b_].num)
+                table[a_][b_].poss[table[i][b_].num - 1] =
                     table[i][b_].num;
-        }
-    return 1;
-}
+    }
+    return 1;
+}
 
-int analysis_squareNode(cell_t ** table) 
+int analysis_squareNode(cell_t ** table)
 {
-    int i = 0;
-    return 1;
-}
+    int i = 0;
+    return 1;
+}
