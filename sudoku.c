a0aca7a50f7a79f8ab4f17e82b8aa32c5995ec31
Anton Kanavalau <akannwetr@gmail.com>
2013-03-09 02:47:18 +0300

    added output support to main

--- sudoku.c
+++ sudoku.c
@@ -1,7 +1,7 @@
 #include "custom_tools.h"
 
-int analysis_rowsNcolumns(cell_t **);
-int analysis_squareNode(cell_t **);
+/*int analysis_rowsNcolumns (cell_t**);*/
+/*int analysis_squareNode (cell_t**);*/
 /*int advanced_analyzis (cell_t**);*/
 
 int main(int argc, char **argv)
@@ -11,6 +11,7 @@ int main(int argc, char **argv)
     int i = 0, j = 0;
     Arguments.output_file = (char *) calloc(255, sizeof(char));
     Arguments.custom_conf_location = (char *) calloc(255, sizeof(char));
+    Arguments.output_enabled = 0;
     if (!options(&Arguments, argc, argv)) {
         printf("Error in parsing parameters.\n");
         return 0;
@@ -20,36 +21,35 @@ int main(int argc, char **argv)
         *(table + i) = (cell_t *) calloc(9, sizeof(cell_t));
     if (!read_conf_file(table, Arguments.custom_conf_location))
         printf("Error in reading config file.\n");
-    for (i = 0; i < 9; i++)
-        for (j = 0; j < 9; j++) {
+    if (Arguments.output_enabled)
+        if (!write_output_file(table, Arguments.output_file))
+            printf("Error in writing file");
+    for (i = 0; i < 9; i++) {
+        for (j = 0; j < 9; j++)
             printf(" %d ", table[i][j].num);
-            if (j == 8)
-                printf("\n");
-        }
+        printf("\n");
+    }
     return (0);                 /* I like it */
 }
 
-
-
-int analysis_rowsNcolumns(cell_t ** table)
+/*int analysis_rowsNcolumns (cell_t** table)
 {
-    int i = 0, j = 0, node_val = 0, a_ = 0, b_ = 0;
-    node_val = table[a_][b_].num;
-    if (!table[a_][b_].num) {
-        for (j = 0; j < 9; j++)
-            if (table[a_][j].num)
-                table[a_][b_].poss[table[a_][j].num - 1] =
-                    table[a_][j].num;
-        for (i = 0; i < 9; i++)
-            if (table[i][b_].num)
-                table[a_][b_].poss[table[i][b_].num - 1] =
-                    table[i][b_].num;
-    }
-    return 1;
-}
+	int i=0,j=0,node_val=0,a_=0,b_=0;
+	node_val=table[a_][b_].num;
+	if (!table[a_][b_].num)
+	{
+		for (j=0;j<9;j++)
+			if (table[a_][j].num)
+				table[a_][b_].poss[table[a_][j].num-1]=table[a_][j].num;
+		for (i=0;i<9;i++)
+			if (table[i][b_].num)
+				table[a_][b_].poss[table[i][b_].num-1]=table[i][b_].num;	
+	}
+	return 1;
+}*/
 
-int analysis_squareNode(cell_t ** table)
+/*int analysis_squareNode (cell_t** table)
 {
-    int i = 0;
-    return 1;
-}
+	int i=0;
+	return 1;
+}*/
